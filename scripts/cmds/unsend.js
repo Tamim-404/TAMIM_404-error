f768a42da84ec4aaba275b8c6d4794fb434ddb38
Sourav549291 <souravkhalua51@gmail.com>
2025-01-30 14:54:12 +0530

    Update unsend.js

--- scripts/cmds/unsend.js
+++ scripts/cmds/unsend.js
@@ -1,7 +1,7 @@
 module.exports = {
 	config: {
 		name: "unsend",
-		aliases: ["un","uns","unsef"],
+		aliases: ["un","uns","Khanki","unsef"],
     version: "1.1",
 		author: "Admin",
 		countDown: 5,
