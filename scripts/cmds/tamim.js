05a66783d31bef1bd78e877feecb6ec3c7a8e8e3
Tamim-404 <itsmetamim02@gmail.com>
2025-03-05 17:58:18 +0600

    Update tamim.js

--- scripts/cmds/tamim.js
+++ scripts/cmds/tamim.js
@@ -15,7 +15,7 @@ module.exports = {
  if (event.body && event.body.toLowerCase() === "tamim") {
  return message.reply({
  body: " 「❥︎----ღ᭄_ʜᴇʏ ..\n❥︎----ღ᭄_  ᴮᴿᴼᵀᴴᴱᴿ❞࿐.♡.\nᥫᩣ ɪᴛ’s ᴛᴀᴍɪᴍ\n\n𝐁𝐎𝐓 𝐎𝐖𝐍𝐄𝐑\n𝐓𝐀𝐌𝐈𝐌♡」",
- attachment: await global.utils.getStreamFromURL("https://i.imgur.com/8vvnaZq.mp4")
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/55RrN7I.mp4")
  });
  }
  }
